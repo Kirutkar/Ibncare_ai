a95f11e3792623a91c7154994d43608183165baa
KIRUTHIKA RAMALINGAM <159514346+Kirutkar@users.noreply.github.com>
2025-04-11 15:04:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import string
 import os
 
 # --- 🌐 Backend URL ---
-BACKEND_URL = "https://ibncare-ai.onrender.com"
+BACKEND_URL = "http://localhost:5000"
 
 # --- 🧠 Initialize Session State ---
 defaults = {
